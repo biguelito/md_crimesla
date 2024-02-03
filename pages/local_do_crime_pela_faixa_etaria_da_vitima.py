3fe82edabe04be4e9ba635311ca6e87ce38eea4d
wnetohr <washingtonbdarocha@gmail.com>
2024-02-03 20:58:48 -0300

    Pagina de local do crime por faixa etaria

---
+++ pages/local_do_crime_pela_faixa_etaria_da_vitima.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import streamlit as st
+import matplotlib.pyplot as plt
+from sql.mysqlconnector import MysqlConnector
+connector = MysqlConnector()
+
+st.title("Quais são os locais mais propensos a crimes envolvendo a faixa etaria da vitima?")
+
+faixas_etarias = ['Selecione','Jovem','Adulto','Idoso']
+faixa_selecionada = st.selectbox(label="Selecione a faixa etária", options=faixas_etarias)
+if faixa_selecionada != "Selecione":
+    st.write("Exibindo resultados na faixa etária: "+faixa_selecionada)
+
+
+
+    
+  
