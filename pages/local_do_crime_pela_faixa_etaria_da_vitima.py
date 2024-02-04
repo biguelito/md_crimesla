5383594eb3130bbdf7e53712bb8bf6f59da8aca9
wnetohr <washingtonbdarocha@gmail.com>
2024-02-03 22:36:12 -0300

    Local do crime pela faixa etaria da vitima update

--- pages/local_do_crime_pela_faixa_etaria_da_vitima.py
+++ pages/local_do_crime_pela_faixa_etaria_da_vitima.py
@@ -4,16 +4,30 @@ import matplotlib.pyplot as plt
 from sql.mysqlconnector import MysqlConnector
 connector = MysqlConnector()
 
+
 st.title("Quais são os locais mais propensos a crimes envolvendo a faixa etaria da vitima?")
 
-faixas_etarias = ['Jovem','Adulto','Idoso']
+faixas_etarias = ['Todos','Jovem','Adulto','Idoso']
 faixa_selecionada = st.selectbox(label="Selecione a faixa etária",
                                  options=faixas_etarias,
                                  placeholder='Selecione')
-if faixa_selecionada != "Selecione":
+if st.button("Processar Dados"):
     st.write("Exibindo resultados na faixa etária: "+faixa_selecionada)
+    resultado = connector.obter_local_perigoso_por_faixa_etaria(faixa_selecionada)
+    df = pd.DataFrame(resultado,columns=["Localidade","Quantidade"])
+    top_10_df = df.head(10)
+    st.header("Tabela de Dados")
+    st.dataframe(df)
+    # Criar gráfico de barras
+    fig, ax = plt.subplots()
+    ax.bar(top_10_df["Localidade"], top_10_df["Quantidade"])
 
+    # Adicionar rótulos e título
+    ax.set_xlabel("Localidade")
+    ax.set_ylabel("Quantidade de Crimes")
+    ax.set_title("Quantidade de Crimes por Local")
 
-
+    # Exibir gráfico no Streamlit
+    st.pyplot(fig)
     
   
